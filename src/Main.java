090c16dede716fc5830c3dd03bba5b38a725cd00
Alex Belkin <dustiklove@yandex.ru>
2023-08-04 15:32:40 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weight = 77; //масса в кг.
+        double height = 1.70; //рост в метрах
+        double index = service.calculate(weight,height);
+
+
+        System.out.println((int)index);
+    }
+}
